cdad73c9b06f841e1ac81aa92a19989c7e281ad6
Justin Cardinal <justin@somuchuptime.com>
2020-01-13 19:49:08 -0600

    Add posts, use templates to render header and posts

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Justin'}
+    posts = [
+        {
+            'author': {'username': 'Jen'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Lindsi'},
+            'body': 'I like New Orleans'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Little Women movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
