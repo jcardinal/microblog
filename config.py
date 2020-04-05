03cc98ff19d89841f8558093bea4d7ee8e5c58b4
Justin Cardinal <justin@somuchuptime.com>
2020-04-04 22:24:29 -0500

    Chapter VII: Error Handling

--- config.py
+++ config.py
@@ -6,3 +6,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('FLASK_MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('FLASK_MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('FLASK_MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('FLASK_MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('FLASK_MAIL_PASSWORD')
+    ADMINS = ['justin@somuchuptime.com']
