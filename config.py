2fc991154e19986d6ece5d1db130c85f454643f6
Justin Cardinal <justin@somuchuptime.com>
2020-04-13 19:13:07 -0500

    Chapter IX: Pagination (and let users post, and create Explore page)

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('FLASK_MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('FLASK_MAIL_PASSWORD')
     ADMINS = ['justin@somuchuptime.com']
+    POSTS_PER_PAGE = 25
